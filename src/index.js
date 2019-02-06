893de94144c20d4f76ebef0361bff0dcdf0734bf
Kurt Van Hal <kurt_van_hal@hotmail.com>
2019-02-06 03:38:51 +0100

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import _ from "lodash";
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 
 function component(){
     let element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    //Klasse toevoegen adhv import
-    element.classList.add('hello');
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = printMe;
 
-    //Icoon toevoegen aaan de bestaande div
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
+    element.appendChild(btn);
 
     return element;
 }
